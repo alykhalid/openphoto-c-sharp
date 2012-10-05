8f2e1ab942f54176edc2e1a7aa0c4e9068acaa1c
Slavo Ingilizov <slavo.ing@gmail.com>
2012-10-05 17:23:27 +0300

    Implemented delete and upload for photos

--- ConsoleSampleApp/Program.cs
+++ ConsoleSampleApp/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
-using System.Text;
 using OpenPhoto;
 
 namespace ConsoleSampleApp
@@ -13,14 +13,39 @@ namespace ConsoleSampleApp
             // to test this API client, you need to specify values for the following properties for your particular
             // OpenPhoto instance. You can view them by going to /manage/apps in the browser
 
-            var testApiKey = "put your consumer key here";
-            var testApiSecret = "put your consumer secret here";
-            var oauthToken = "put your oauth token here";
-            var oauthSecret = "put your oauth secret here";
+            var testApiKey = "your_api_key";
+            var testApiSecret = "your_api_secret";
+            var oauthToken = "your_oauth_token";
+            var oauthSecret = "your_oauth_secret";
 
             var client = new OpenPhotoClient("http://yourdomain.com", testApiKey, testApiSecret, oauthToken, oauthSecret);
+
             var photoList = client.Photos.GetList();
-            var singlePhoto = client.Photos.Get("id-of-photo");
+            foreach (var photo in photoList.Result)
+            {
+                Console.WriteLine("Photo ID: " + photo.Id + "\tPath: " + photo.PathBase);
+            }
+
+            var singlePhoto = client.Photos.Get(photoList.Result.First().Id);
+            Console.WriteLine();
+            Console.WriteLine("The first photo from the list is: " + singlePhoto.Result.Id);
+
+            var deleteResult = client.Photos.Delete(singlePhoto.Result.Id);
+            Console.WriteLine();
+            Console.WriteLine("Tried to delete first photo. Result: " + deleteResult.Result);
+
+            var secondPhoto = photoList.Result.Skip(1).Take(1).First();
+            var updateSecondPhoto = client.Photos.Update(secondPhoto.Id, new OpenPhoto.Model.Photo()
+            {
+                Title = "Updated title"
+            });
+
+            Image imageContent = Image.FromFile("C:\\local\\path\\to\\your\\photo.jpg");
+            MemoryStream stream = new MemoryStream();
+            imageContent.Save(stream, System.Drawing.Imaging.ImageFormat.Jpeg);
+            var uploadResult = client.Photos.Upload(stream.ToArray(), "photo.jpg");
+            
+            Console.ReadLine();
         }
     }
 }
--- OpenPhoto/Endpoints/PhotoEndpoint.cs
+++ OpenPhoto/Endpoints/PhotoEndpoint.cs
@@ -37,11 +37,40 @@ namespace OpenPhoto.Endpoints
             return response.Data;
         }
 
-        public ResponseEnvelope<Photo> Delete(string id)
+        public ResponseEnvelope<bool> Delete(string id)
         {
             var request = new RestRequest(PhotoEndpoint.EndpointUrlSingular + "/" + id + "/delete.json", Method.POST);
+            var response = this.restClient.Execute<ResponseEnvelope<bool>>(request);
+
+            return response.Data;
+        }
+
+        public ResponseEnvelope<Photo> Update(string id, Photo newPhoto)
+        {
+            var request = new RestRequest(PhotoEndpoint.EndpointUrlSingular + "/" + id + "/update.json", Method.POST);
+
+            request.Parameters.Add(new Parameter() { Name = "permission", Value = newPhoto.Permission, Type = ParameterType.GetOrPost });
+            request.Parameters.Add(new Parameter() { Name = "title", Value = newPhoto.Title, Type = ParameterType.GetOrPost });
+            request.Parameters.Add(new Parameter() { Name = "description", Value = newPhoto.Description, Type = ParameterType.GetOrPost });
+            request.Parameters.Add(new Parameter() { Name = "dateUploaded", Value = newPhoto.DateUploaded, Type = ParameterType.GetOrPost });
+            request.Parameters.Add(new Parameter() { Name = "dateTaken", Value = newPhoto.DateTaken, Type = ParameterType.GetOrPost });
+            request.Parameters.Add(new Parameter() { Name = "license", Value = newPhoto.License, Type = ParameterType.GetOrPost });
+            request.Parameters.Add(new Parameter() { Name = "latitude", Value = newPhoto.Latitude, Type = ParameterType.GetOrPost });
+            request.Parameters.Add(new Parameter() { Name = "longitude", Value = newPhoto.Longitude, Type = ParameterType.GetOrPost });
+
             var response = this.restClient.Execute<ResponseEnvelope<Photo>>(request);
 
+            return response.Data;
+        }
+
+        public ResponseEnvelope<Photo> Upload(byte[] photoData, string fileName)
+        {
+            var request = new RestRequest(PhotoEndpoint.EndpointUrlSingular + "/upload.json", Method.POST);
+
+            var fileToUpload = FileParameter.Create("photo", photoData, fileName);
+            request.Files.Add(fileToUpload);
+
+            var response = this.restClient.Execute<ResponseEnvelope<Photo>>(request);
             return response.Data;
         }
 
